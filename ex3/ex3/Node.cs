641e91bb95350feb04ff205f7ff8a4a9763c5128
unknown <lior.jakoby46@gmail.com>
2024-05-05 20:21:29 +0300

    Added Node class

---
+++ ex3/ex3/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex3
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            this.Value = value;
+            this.Next = null;
+        }
+        public Node(int value, Node next)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+    }
+}
