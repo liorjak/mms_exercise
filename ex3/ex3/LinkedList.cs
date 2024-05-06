d63ee802a3c0ea4928fcd235c6ee19a4e1e73641
unknown <lior.jakoby46@gmail.com>
2024-05-06 11:14:42 +0300

    Added private void Append(int value) method

--- ex3/ex3/LinkedList.cs
+++ ex3/ex3/LinkedList.cs
@@ -8,6 +8,24 @@ namespace ex3
     class LinkedList<T>
     {
         public Node<int> Head { get; set; }
+        
+        private void Append(int value)
+        {
+            if(Head.Value == null)
+            {
+                Head.Value = value;
+            }
+            else
+            {
+                Node<int> node = new Node<int>(value);
+                node.Next = Head.Next;
+                Head.Value = node.Value;
+            }
+        }
 
+        private void Prepend(int value)
+        {
+            Head.Value = value;
+        }
     }
 }
