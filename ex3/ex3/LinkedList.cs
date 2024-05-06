0ae0bb034d8b62b5b7e29cb64cd7479c35a8a5e6
unknown <lior.jakoby46@gmail.com>
2024-05-06 11:11:07 +0300

    Added private void Prepend(int value) method

---
+++ ex3/ex3/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ex3;
+
+namespace ex3
+{
+    class LinkedList<T>
+    {
+        public Node<int> Head { get; set; }
+
+    }
+}
--- ex3/ex3/Node.cs
+++ ex3/ex3/Node.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ex3;
 
 namespace ex3
 {
-    class Node
+    class Node<T>
     {
         public int Value { get; set; }
-        public Node Next { get; set; }
+        public Node<T> Next { get; set; }
 
         public Node(int value)
         {
             this.Value = value;
             this.Next = null;
         }
-        public Node(int value, Node next)
+        public Node(int value, Node<T> next)
         {
             this.Value = value;
             this.Next = next;
