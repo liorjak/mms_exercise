4e521048c96889e5e3fba3eaccd4569d4498b4c6
unknown <lior.jakoby46@gmail.com>
2024-05-11 19:18:27 +0300

    Added part_4 diractory

---
+++ part_4/ex4_2048_game/ex4_2048_game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex4_2048_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
